69c1e4501e02763e7fcd488b7737dcdda79df076
etl-datago <etl@datago.com.br>
2021-10-13 15:18:58 -0300

    Create ProtheusapiApplicationTests.java

---
+++ src/test/java/com/api/protheus/ProtheusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.protheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtheusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
